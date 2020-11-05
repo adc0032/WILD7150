0c4735c161183e5cd481e95424ab35d012298a9b
Amanda Clark <adc0032@auburn.edu>
2020-11-04 21:23:48 -0600

    updating package to include tutorial

---
+++ R/docs.R
@@ -0,0 +1,33 @@
+#' @title agingtrees
+#' @description Dataset 3: Repeated meausures of tree height of 16 trees over 10 yrs to understand growth trajectory
+#' @format A data frame with 176 rows and 3 variables:
+#' \describe{
+#'   \item{\code{Individual}}{integer Tree ID}
+#'   \item{\code{Age}}{integer Age in years}
+#'   \item{\code{Size}}{double Height in meters }
+#'}
+#' @source \url{http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assign6a.csv}
+"agingtrees"
+
+#' @title hormonaltrees
+#' @description Dataset 2: Average tree height per plot, based on plot exposure to fertilizer and/or hormones
+#' @format A data frame with 32 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Field}}{integer Field ID}
+#'   \item{\code{Fertilizer}}{integer Fertilizer added or not added to the field(1/0)}
+#'   \item{\code{Hormone}}{integer Hormone added or not added to the field(1/0)}
+#'   \item{\code{Size}}{double Average tree height in centimeters}
+#'}
+#' @source \url{http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assign6c.csv}
+"hormonaltrees"
+
+#' @title pickygrasshoppers
+#' @description Dataset 1: Grasshopper density (grasshopper/hectare) in response to controlled burn or fertilizer
+#' @format A data frame with 24 rows and 3 variables:
+#' \describe{
+#'   \item{\code{Field}}{integer FIELD ID}
+#'   \item{\code{Plot}}{integer Treatments to Plot: Control (nothing), dBurn (Burned), Fertilize (Fertilized) }
+#'   \item{\code{Density}}{double Hopper Density: Number of grasshoppers per hectare}
+#'}
+#' @source \url{http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assign6b.csv}
+"pickygrasshoppers"
