16f7b7183f0f5e1aa9d5c4377fa9f4037718a493
Amanda D. Clark <adc0032@auburn.edu>
2021-05-13 19:34:17 -0500

    adding new dataset lotuscountry - lotus size with independent variables of elevation and country

--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -9,12 +9,15 @@ usethis::use_data(datasets, overwrite = TRUE)
 pickygrasshoppers <- read.csv("http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assign6b.csv")
 
 # Dataset 2: Average tree height data, based on exposure to fertilizer and/or hormones
-hormonaltrees <-read.csv("http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assign6c.csv")
+hormonaltrees <- read.csv("http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assign6c.csv")
 
-# Dataset 3: Repeated meausures of tree height of 16 trees over 10 yrs to understand growth trajectory
+# Dataset 3: Repeated measures of tree height of 16 trees over 10 yrs to understand growth trajectory
 agingtrees <- read.csv("http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assign6a.csv")
+
+# Dataset 4: Lotus size (grams) for flowers in 3 different countries (w/ continuous variables for elevation and latitude)
+lotuscountry <- read.csv("http://webhome.auburn.edu/~tds0009/WILD7150/Assignments/Assignment4.csv")
 
 usethis::use_data(agingtrees, overwrite = TRUE)
 usethis::use_data(hormonaltrees, overwrite = TRUE)
 usethis::use_data(pickygrasshoppers, overwrite = TRUE)
-
+usethis::use_data(lotuscountry, overwrite = TRUE)
